3420a0048fa2b132701919154e313dc0dfec3001
Bruno_Gust <bruno.1331@hotmail.com>
2024-05-26 21:53:04 -0500

    About us

--- src/components/Aboutus.vue
+++ src/components/Aboutus.vue
@@ -3,10 +3,54 @@
     <h1>Educational Virtual Assistant</h1>
     <div class="about-message">
       <p>
-        ¡Hola! Soy EVA, tu asistente virtual educativo. Estoy aquí para ayudarte a navegar por nuestra plataforma y sacar el máximo provecho de tu experiencia de aprendizaje. ¿Listo para empezar? Simplemente dale al botón de 'Iniciar Sesión' si ya tienes una cuenta o 'Crear Cuenta' si eres nuevo aquí. ¡Estoy emocionado de acompañarte en tu viaje de aprendizaje!
-      </p>
+        ¡Bienvenido a la sección 'Conócenos'! Permíteme presentarte a los creadores detrás de esta plataforma. Somos un equipo apasionado de desarrolladores dedicados a proporcionarte la mejor experiencia de aprendizaje en línea. Desde el diseño hasta la implementación, hemos trabajado arduamente para crear una plataforma que se adapte a tus necesidades educativas. ¡Esperamos que disfrutes de lo que hemos creado para ti!</p>
     </div>
-  </div>
+
+    <div class="photo-container">
+      <div class="photo-manuel">
+      </div>
+      
+      <div class="photo-pool">
+
+      </div>
+      
+      <div class="photo-bruno">
+
+      </div>
+    
+    </div>
+    </div>
+
 
 
 </template>
+
+
+<style scoped>
+.about-container {
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  background-color: #e0f7fa;
+  bottom: 12rem;
+}
+.about-container h1{
+  width: 26rem;
+  margin-left: 6rem;
+  align-items: center;
+  justify-content: center;
+
+}
+.about-message{
+  background-color: #000;
+  color: #fff;
+  padding: 0.5rem;
+  border-radius: 10px;
+  text-align: center;
+  position: relative;
+  max-width: 600px;
+}
+
+
+
+</style>
